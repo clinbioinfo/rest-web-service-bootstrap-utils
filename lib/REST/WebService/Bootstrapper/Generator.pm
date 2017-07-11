114f77a84dbb04b56c11064d1c7f7663deac5356
Jaideep Sundaram <sundaramj@medimmune.com>
2017-07-11 16:47:16 -0400

    Latest.  Adds support for autogenerating the methods in the App, Manager and DBUtil modules.

--- lib/REST/WebService/Bootstrapper/Generator.pm
+++ lib/REST/WebService/Bootstrapper/Generator.pm
@@ -9,18 +9,35 @@ use FindBin;
 use Term::ANSIColor;
 use FindBin;
 use Template;
-
+use File::Slurp;
 
 use constant TRUE  => 1;
 use constant FALSE => 0;
 
 use constant DEFAULT_TEST_MODE => TRUE;
 
+use constant DEFAULT_DESCRIPTION => 'N/A';
+
 use constant DEFAULT_VERBOSE => TRUE;
 
 use constant DEFAULT_USERNAME => getlogin || getpwuid($<) || $ENV{USER} || "sundaramj";
 
 use constant DEFAULT_OUTDIR => '/tmp/' . DEFAULT_USERNAME . '/' . File::Basename::basename($0) . '/' . time();
+
+use constant DEFAULT_APP_METHOD_TEMPLATE_FILE => "$FindBin::Bin/../template/app_method_perl_tmpl.tt";
+
+use constant DEFAULT_MANAGER_METHOD_TEMPLATE_FILE => "$FindBin::Bin/../template/manager_method_perl_tmpl.tt";
+
+use constant DEFAULT_SQLITE_DBUTIL_METHOD_TEMPLATE_FILE => "$FindBin::Bin/../template/sqlite_dbutil_method_perl_tmpl.tt";
+
+use constant DEFAULT_MONGODB_DBUTIL_METHOD_TEMPLATE_FILE => "$FindBin::Bin/../template/mongodb_dbutil_method_perl_tmpl.tt";
+
+use constant DEFAULT_ORACLE_DBUTIL_METHOD_TEMPLATE_FILE => "$FindBin::Bin/../template/oracle_dbutil_method_perl_tmpl.tt";
+
+use constant DEFAULT_POSTGRESQL_DBUTIL_METHOD_TEMPLATE_FILE => "$FindBin::Bin/../template/postgresql_dbutil_method_perl_tmpl.tt";
+
+use constant DEFAULT_MYSQL_METHOD_TEMPLATE_FILE => "$FindBin::Bin/../template/mysql_dbutil_method_perl_tmpl.tt";
+
 
 my $modulate_template_file_list = [
     'template/namespace_Logger_tmpl.tt',
@@ -115,6 +132,77 @@ has 'copyright' => (
     required => FALSE
     );
 
+has 'app_method_template_file' => (
+    is       => 'rw',
+    isa      => 'Str',
+    writer   => 'setAppMethodTemplateFile',
+    reader   => 'getAppMethodTemplateFile',
+    required => FALSE,
+    default  => DEFAULT_APP_METHOD_TEMPLATE_FILE
+    );
+
+has 'manager_method_template_file' => (
+    is       => 'rw',
+    isa      => 'Str',
+    writer   => 'setManagerMethodTemplateFile',
+    reader   => 'getManagerMethodTemplateFile',
+    required => FALSE,
+    default  => DEFAULT_MANAGER_METHOD_TEMPLATE_FILE
+    );
+
+has 'mongodb_dbutil_method_template_file' => (
+    is       => 'rw',
+    isa      => 'Str',
+    writer   => 'setMongoDBDBUtilMethodTemplateFile',
+    reader   => 'getMongoDBDBUtilMethodTemplateFile',
+    required => FALSE,
+    default  => DEFAULT_MONGODB_DBUTIL_METHOD_TEMPLATE_FILE
+    );
+
+has 'sqlite_dbutil_method_template_file' => (
+    is       => 'rw',
+    isa      => 'Str',
+    writer   => 'setSQLiteDBUtilMethodTemplateFile',
+    reader   => 'getSQLiteDBUtilMethodTemplateFile',
+    required => FALSE,
+    default  => DEFAULT_SQLITE_DBUTIL_METHOD_TEMPLATE_FILE
+    );
+
+has 'oracle_dbutil_method_template_file' => (
+    is       => 'rw',
+    isa      => 'Str',
+    writer   => 'setOracleDBUtilMethodTemplateFile',
+    reader   => 'getOracleDBUtilMethodTemplateFile',
+    required => FALSE,
+    default  => DEFAULT_ORACLE_DBUTIL_METHOD_TEMPLATE_FILE
+    );
+
+has 'postgresql_dbutil_method_template_file' => (
+    is       => 'rw',
+    isa      => 'Str',
+    writer   => 'setPostgresqlDBUtilMethodTemplateFile',
+    reader   => 'getPostgresqlDBUtilMethodTemplateFile',
+    required => FALSE,
+    default  => DEFAULT_POSTGRESQL_DBUTIL_METHOD_TEMPLATE_FILE
+    );
+
+has 'mysql_dbutil_method_template_file' => (
+    is       => 'rw',
+    isa      => 'Str',
+    writer   => 'setMySQLDBUtilMethodTemplateFile',
+    reader   => 'getMySQLDBUtilMethodTemplateFile',
+    required => FALSE,
+    default  => DEFAULT_MYSQL_METHOD_TEMPLATE_FILE
+    );
+
+has 'dbutil_type' => (
+    is       => 'rw',
+    isa      => 'Str',
+    writer   => 'setDBUtilType',
+    reader   => 'getDBUtilType',
+    required => FALSE
+    );
+
 sub getInstance {
 
     if (!defined($instance)){
@@ -166,11 +254,212 @@ sub generate {
         }
     }
 
-#    $self->_generate_app_module($record_list);
-
- #   $self->_generate_dbutil_module($record_list);
+    $self->_generate_methods($record_list);
 
     $self->_generate_modules();
+}
+
+sub _generate_methods {
+
+    my $self = shift;
+    my ($record_list) = @_;
+
+    $self->{_app_method_content_list} = [];
+
+    $self->{_manager_method_content_list} = [];
+
+    my $default_description = DEFAULT_DESCRIPTION;
+
+    foreach my $record (@{$record_list}){
+
+        my $url = $record->getURL();
+        if (!defined($url)){
+            $self->{_logger}->logconfess("url was not defined for Record : " . Dumper $record);
+        }
+
+        my $type = $record->getType();
+        if (!defined($type)){
+            $self->{_logger}->logconfess("type was not defined for Record : " . Dumper $record);
+        }
+
+        my $desc = $record->getDesc();
+        if (!defined($desc)){
+            $desc = $default_description;
+            $self->{_logger}->warn("desc was not defined and therefore was set to default '$desc'");
+        }
+
+        my $name = $record->getName();
+        if (!defined($name)){
+            $self->{_logger}->logconfess("name was not defined for Record : " . Dumper $record);
+        }
+
+        my $method_name = lc($name);
+
+        $method_name =~ s|\s+|_|g;
+
+        $method_name =~ s|\-|_|g;
+
+        my $sql = $record->getSQL();
+        if (!defined($sql)){
+            $self->{_logger}->logconfess("sql was not defined for Record : " . Dumper $sql);
+        }
+
+        $self->_get_app_method_content($method_name, $url, $desc);
+
+        $self->_get_manager_method_content($method_name, $url, $desc, $type);
+
+        $self->_set_dbutil_type($type);
+
+        $self->_get_dbutil_method_content($method_name, $url, $sql, $desc, $type);
+    }
+}
+
+sub _set_dbutil_type {
+
+    my $self = shift;
+    my ($type) = @_;
+
+    $type = lc($type);
+
+    my $current_type = $self->getDBUtilType();
+    if (!defined($current_type)){
+        $self->setDBUtilType($type);
+    }
+    else {
+        if ($current_type ne $type){
+            $self->{_logger}->logconfess("current_type '$current_type' does not match type '$type'");
+        }
+    }
+}
+
+sub _get_app_method_content {
+
+    my $self = shift;
+    my ($method_name, $url, $desc) = @_;
+    
+    my $template_file = $self->getAppMethodTemplateFile();
+    if (!defined($template_file)){
+        $self->{_logger}->logconfess("template_file was not defined");
+    }
+
+    my $lookup = {
+        url => $url,
+        desc => $desc,
+        method_name => $method_name
+    };
+
+    my $content = $self->_generate_content_from_template($template_file, $lookup);
+
+    push(@{$self->{_app_method_content_list}}, $content);
+}
+
+sub _get_manager_method_content {
+
+    my $self = shift;
+    my ($method_name, $url, $desc, $type) = @_;
+    
+    my $template_file = $self->getManagerMethodTemplateFile();
+    if (!defined($template_file)){
+        $self->{_logger}->logconfess("template_file was not defined");
+    }
+
+    my $lookup = {
+        url => $url,
+        desc => $desc,
+        type => $type,
+        method_name => $method_name
+    };
+
+    my $content = $self->_generate_content_from_template($template_file, $lookup);
+
+    push(@{$self->{_manager_method_content_list}}, $content);
+}
+
+sub _get_dbutil_method_content {
+
+    my $self = shift;
+    my ($method_name, $url, $sql, $desc, $type) = @_;
+    
+    my $template_file;
+
+    if (lc($type) eq 'oracle'){
+        $template_file = $self->getOracleDBUtilMethodTemplateFile();
+    }
+    elsif (lc($type) eq 'mongodb'){
+        $template_file = $self->getMongoDBDBUtilMethodTemplateFile();
+    }
+    elsif (lc($type) eq 'sqlite'){
+        $template_file = $self->getSQLiteDBUtilMethodTemplateFile();
+    }
+    elsif (lc($type) eq 'postgresql'){
+        $template_file = $self->getPostgresqlDBUtilMethodTemplateFile();
+    }
+    elsif (lc($type) eq 'mysql'){
+        $template_file = $self->getMySQLDBUtilMethodTemplateFile();
+    }
+    else {
+        $self->{_logger}->logconfess("type '$type' is not supported");
+    }
+    
+    if (!defined($template_file)){
+        $self->{_logger}->logconfess("template_file was not defined");
+    }
+
+    my $lookup = {
+        url  => $url,
+        desc => $desc,
+        sql  => $sql,
+        method_name => $method_name,
+
+    };
+
+    my $content = $self->_generate_content_from_template($template_file, $lookup);
+
+    if (lc($type) eq 'oracle'){
+        push(@{$self->{_oracle_dbutil_method_content_list}}, $content);
+    }
+    elsif (lc($type) eq 'mongodb'){
+        push(@{$self->{_mongodb_dbutil_method_content_list}}, $content);
+    }
+    elsif (lc($type) eq 'sqlite'){
+        push(@{$self->{_sqlite_dbutil_method_content_list}}, $content);
+    }
+    elsif (lc($type) eq 'postgresql'){
+        push(@{$self->{_postgresql_dbutil_method_content_list}}, $content);
+    }
+    elsif (lc($type) eq 'mysql'){
+        push(@{$self->{_mysql_dbutil_method_content_list}}, $content);
+    }
+    else {
+        $self->{_logger}->logconfess("type '$type' is not supported");
+    }
+}
+
+sub _generate_content_from_template {
+
+    my $self = shift;
+    my ($template_file, $lookup) = @_;
+
+    my $tt = new Template({ABSOLUTE => 1});
+    if (!defined($tt)){
+        $self->{_logger}->logconfess("Could not instantiate TT");
+    }
+
+    my $tmp_outfile = $self->getOutdir() . '/out.pm';
+
+    $tt->process($template_file, $lookup, $tmp_outfile) || $self->{_logger}->logconfess("Encountered the following Template::process error:" . $tt->error());
+    
+    $self->{_logger}->info("Wrote temporary output file '$tmp_outfile'");
+
+    my @lines = read_file($tmp_outfile);
+
+    my $content = join("", @lines);
+
+    unlink($tmp_outfile) || $self->{_logger}->logconfess("Could not unlink temporary output file '$tmp_outfile' : $!");
+
+    $self->{_logger}->info("temporary output file '$tmp_outfile' was removed");
+
+    return $content;
 }
 
 sub _generate_modules {
@@ -209,6 +498,8 @@ sub _generate_modules {
 
         ## Example: template/namespace_Postgresql_DBUtil_tmpl.tt
 
+        my $template_file_basename = File::Basename::basename($template_file);
+
         if ($template_file =~ m|template/namespace_(\S+)_tmpl\.tt|){
 
             my $path = $1; ## e.g. Postgresql_DBUtil
@@ -242,7 +533,65 @@ sub _generate_modules {
                 namespace => $namespace,
                 full_namespace => $full_namespace
             };
+
+
+            if ($template_file_basename eq 'namespace_App_tmpl.tt'){
+                
+                $self->{_logger}->info("Going to load app methods");
+
+                $final_lookup->{app_methods} = join("\n\n", @{$self->{_app_method_content_list}});
+            }
+            elsif ($template_file_basename eq 'namespace_Manager_tmpl.tt'){
+                
+                $self->{_logger}->info("Going to load manager methods");
+
+                $final_lookup->{dbutil_type}  = $self->getDBUtilType();
+
+                $final_lookup->{manager_methods} = join("\n\n", @{$self->{_manager_method_content_list}});
+            }
+            elsif ($template_file_basename eq 'namespace_MongoDB_DBUtil_tmpl.tt'){
+
+                if (exists $self->{_mongodb_dbutil_method_content_list}){
+                    $self->{_logger}->info("Going to load mongodb methods");
+
+                    $final_lookup->{mongodb_methods} = join("\n\n", @{$self->{_mongodb_dbutil_method_content_list}});
+                }
+            }
+            elsif ($template_file_basename eq 'namespace_SQLite_DBUtil_tmpl.tt'){
+             
+                if (exists $self->{_sqlite_dbutil_method_content_list}){
+                    $self->{_logger}->info("Going to load sqlite methods");
+
+                    $final_lookup->{sqlite_methods} = join("\n\n", @{$self->{_sqlite_dbutil_method_content_list}});
+                }
+            }
+            elsif ($template_file_basename eq 'namespace_Oracle_DBUtil_tmpl.tt'){
+             
+                if (exists $self->{_oracle_dbutil_method_content_list}){
+                    $self->{_logger}->info("Going to load oracle methods");
+
+                    $final_lookup->{oracle_methods} = join("\n\n", @{$self->{_oracle_dbutil_method_content_list}});
+                }
+            }
+            elsif ($template_file_basename eq 'namespace_Postgresql_DBUtil_tmpl.tt'){
             
+                if (exists $self->{_postgresql_dbutil_method_content_list}){    
+                    $self->{_logger}->info("Going to load postgresql methods");
+
+                    $final_lookup->{postgresql_methods} = join("\n\n", @{$self->{_postgresql_dbutil_method_content_list}});
+                }
+            }
+            elsif ($template_file_basename eq 'namespace_MySQL_DBUtil_tmpl.tt'){
+            
+                if (exists $self->{_mysql_dbutil_method_content_list}){
+                    $self->{_logger}->info("Going to load mysql methods");
+
+                    $final_lookup->{mysql_methods} = join("\n\n", @{$self->{_mysql_dbutil_method_content_list}});
+                }
+            }
+
+
+
             my $tt = new Template({ABSOLUTE => 1});
             if (!defined($tt)){
                 $self->{_logger}->logconfess("Could not instantiate TT");
